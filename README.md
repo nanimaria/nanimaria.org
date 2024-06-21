2702be4e5cdeafebe7b754ea62149dbb5166a6a4
Dipak Pathak <dipak@techasoft.com>
2024-06-21 11:42:02 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# nanimaria.org
---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+window.addEventListener("scroll", function () {
+    var topheader = document.querySelector(".top_header");
+    topheader.classList.toggle("active-bg", window.scrollY > 120);
+  });
+
+// carousel
+$(document).ready(function(){
+  $(".owl-carousel.owl-1").owlCarousel({
+    center: true,
+    items:1,
+    loop:true,
+    nav:false,
+    dots:true,
+    eachDots:true,
+    autoplay:true,
+    smartSpeed:1200,
+    margin:10,
+    responsive:{
+        600:{
+            items:1
+        }
+    },navText:["<i class='fa fa-angle-left'></i>",
+    "<i class='fa fa-angle-right' ></i>"]
+  });
+});
